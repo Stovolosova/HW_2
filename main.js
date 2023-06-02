2e4a2fbf04322dfa30677a2dcd6e0f882085b36d
stovolosova <stovolosova.helen@gmail.com>
2023-06-02 15:18:40 +0200

    added files

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+let userName = prompt ("What is your name?");
+if (userName == "John") {
+    alert ("Hello, John! How are you?");
+} else {
+    alert ("Hello! How are you?");
+}
+// Math . 
+  let a=100, b=20
+  console.log (a + b);
+  console.log (a-b);
+  console.log (a*b);
+  console.log (a/b); 
+
+  // Boolean
+ c = '5' == 5;
+ z = '5' === 5;
+
+ // Array
+
+ const num = [5, 10, 20];
+    let total = num.reduce(function(a, b) {
+        return a + b;
+    });
+    
+ console.log(total / num.length);
+ 
+
+ let number = '12345';
+ console.log(number.split(''));
+ 
